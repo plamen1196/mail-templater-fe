ed42bce4f00988e6022ac791fbc35eaf23c900eb
tachojelev <tachojelev@gmail.com>
2022-01-27 18:14:04 +0200

    Enable create template functionality

--- src/api/email-templater-api.ts
+++ src/api/email-templater-api.ts
@@ -6,7 +6,7 @@
     public static readonly HISTORY: string = 'http://localhost:8080/history';
     public static readonly SEND_MAILS: string = 'http://localhost:8080/send-mails';
     public static readonly PREVIEW_MAILS: string = 'http://localhost:8080/preview-mails';
-    public static readonly ADD_TEMPLATE: string = 'http://localhost:8080/add-template';
-    public static readonly FETCH_TEMPLATES: string = 'http://localhost:8080/fetch-templates';
+    public static readonly ADD_TEMPLATE: string = 'http://localhost:8080/templates';
+    public static readonly FETCH_TEMPLATES: string = 'http://localhost:8080/templates';
     public static readonly DELETE_TEMPLATE: string = 'http://localhost:8080/';
 }
--- src/template-dashboard/view-templates.component.ts
+++ src/template-dashboard/view-templates.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
-
+import { EmailTemplate } from 'src/models/email-template';
+import { TemplateService } from 'src/services/template.service';
+import { CreateTemplateComponent } from './create-template/create-template.component';
 
 @Component({
   selector: 'app-view-templates',
@@ -10,9 +14,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewTemplatesComponent implements OnInit {
 
-  constructor() { }
+  emailTemplates: Array<EmailTemplate> = [];
+
+  constructor(
+    private dialog: MatDialog,
+    private templateService: TemplateService,
+    private snackbar: MatSnackBar) { }
 
   ngOnInit(): void {
+    this.fetchTemplates();
   }
 
+  onCreateTemplate(): void {
+    const dialogRef = this.dialog.open(CreateTemplateComponent, {
+      data: {},
+      disableClose: true,
+      width: '700px',
+      minHeight: 500,
+      maxHeight: 800,
+      autoFocus: false
+    });
+
+    dialogRef.afterClosed().subscribe(({ success, cancelClicked }) => {
+      if (!cancelClicked) {
+        const message = success ? "Template created successfully!" : "ERROR when creating template!";
+        this.snackbar.open(message, undefined, {
+          duration: 3000
+        });
+
+        this.fetchTemplates();
+      }
+    });
+  }
+
+  // onEditTemplate(emailTemplate: EmailTemplate): void {
+  //   const dialogRef = this.dialog.open(EditTemplateComponent, {
+  //     data: { emailTemplate: emailTemplate },
+  //     disableClose: true,
+  //     width: '700px',
+  //     minHeight: 500,
+  //     maxHeight: 800,
+  //     autoFocus: false
+  //   });
+
+  //   dialogRef.afterClosed().subscribe(({ success, cancelClicked }) => {
+  //     if (!cancelClicked) {
+  //       const message = success ? "Template edited successfully!" : "ERROR when editing template!";
+  //       this.snackbar.open(message, null, {
+  //         duration: 3000
+  //       });
+
+  //       this.fetchTemplates();
+  //     }
+  //   });
+  // }
+
+  // onDeleteTemplate(emailTemplate: EmailTemplate): void {
+  //   const dialogRef = this.dialog.open(DeleteTemplateComponent, {
+  //     data: { emailTemplate: emailTemplate },
+  //     disableClose: true,
+  //     width: '500px',
+  //     minHeight: 200,
+  //     maxHeight: 800,
+  //     autoFocus: false
+  //   });
+
+  //   dialogRef.afterClosed().subscribe(({ success, cancelClicked }) => {
+  //     if (!cancelClicked) {
+  //       const message = success ? "Template deleted successfully!" : "ERROR when deleting template!";
+  //       this.snackbar.open(message, null, {
+  //         duration: 3000
+  //       });
+
+  //       this.fetchTemplates();
+  //     }
+  //   });
+  // }
+
+  private fetchTemplates(): void {
+    this.templateService.getTemplates().subscribe((response: Array<EmailTemplate>) => {
+      this.emailTemplates = response;
+    });
+  }
 }
