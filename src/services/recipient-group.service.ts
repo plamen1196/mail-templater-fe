fbcdb8018532da3d775a80548f41288c28d06485
tachojelev <tachojelev@gmail.com>
2022-02-18 18:59:46 +0200

    Implement api for crud between recipients and groups

--- src/services/recipient-group.service.ts
+++ src/services/recipient-group.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 
 import { EmailTemplaterApi } from 'src/api/email-templater-api';
 import { RecipientGroupResource } from 'src/models/recipient-groups/recipient-group-resource';
+import { RecipientResource } from 'src/models/recipients/recipient-resource';
 
 @Injectable({
   providedIn: 'root'
@@ -31,5 +32,20 @@ export class RecipientGroupService {
   editRecipient(id: number, recipientGroupResource: RecipientGroupResource): Observable<RecipientGroupResource> {
     const headers = new HttpHeaders().set('content-type', 'application/json');
     return this.httpClient.patch<any>(EmailTemplaterApi.PATCH_RECIPIENT_GROUP + `${id}`, recipientGroupResource, { headers });
+  }
+
+  getRecipientsOfRecipientGroupById(id: number): Observable<Array<RecipientResource>> {
+    const headers = new HttpHeaders().set('content-type', 'application/json');
+    return this.httpClient.get<Array<RecipientResource>>(EmailTemplaterApi.RECIPIENT_GROUP_API + `/${id}/recipients`, { headers });
+  }
+
+  addRecipientToRecipientGroup(recipientGroupId: number, recipientId: number): Observable<Array<RecipientResource>> {
+    const headers = new HttpHeaders().set('content-type', 'application/json');
+    return this.httpClient.post<Array<RecipientResource>>(EmailTemplaterApi.RECIPIENT_GROUP_API + `/${recipientGroupId}/recipients/${recipientId}`, null, { headers });
+  }
+
+  removeRecipientFromRecipientGroup(recipientGroupId: number, recipientId: number): Observable<Array<RecipientResource>> {
+    const headers = new HttpHeaders().set('content-type', 'application/json');
+    return this.httpClient.delete<Array<RecipientResource>>(EmailTemplaterApi.RECIPIENT_GROUP_API + `/${recipientGroupId}/recipients/${recipientId}`, { headers });
   }
 }
